3021ce7b9ac3b6a9bae0b411b29b449b0fc05ac9
ngenovictor <ngenovictor321@gmail.com>
2017-09-30 02:15:30 +0300

    update loginactivity

--- app/src/main/java/ke/co/carepay/mtiba/ui/LoginActivity.java
+++ app/src/main/java/ke/co/carepay/mtiba/ui/LoginActivity.java
@@ -2,14 +2,23 @@ package ke.co.carepay.mtiba.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import ke.co.carepay.mtiba.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText loginUserName;
+    private EditText loginPassword;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginUserName = (EditText) findViewById(R.id.loginUserName);
+        loginPassword = (EditText) findViewById(R.id.loginPassword);
+        loginButton = (Button) findViewById(R.id.loginButton);
     }
 }
